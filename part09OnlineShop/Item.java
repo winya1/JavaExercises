8ed9d23e778107bbe5352f15ed7e5e8e2e888b31
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-14 14:52:21 -0400

    Create Item.java

---
+++ part09OnlineShop/Item.java
@@ -0,0 +1,24 @@
+public class Item {
+    private String product;
+    private int qty;
+    private int unitPrice;
+
+    public Item(String product, int qty, int unitPrice) {
+        this.product = product;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public int price() {
+        return unitPrice * qty;
+    }
+
+    public void increaseQuantity() {
+        this.qty = this.qty + 1;
+    }
+
+    @Override
+    public String toString() {
+        return this.product + ": " + this.qty;
+    }
+ }
