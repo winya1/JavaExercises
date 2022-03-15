b62f7f9e8f5f1cde99e268615992bb8be8476e8a
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-15 09:55:20 -0400

    Create Cat.java

---
+++ part09Animals/Cat.java
@@ -0,0 +1,20 @@
+public class Cat extends Animal implements NoiseCapable {
+    private String name;
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    public Cat() {
+        super("Cat");
+    }
+
+    public void purr() {
+        System.out.println(super.getName() + " purrs");
+    }
+
+    @Override
+    public void makeNoise() {
+        purr();
+    }
+}
