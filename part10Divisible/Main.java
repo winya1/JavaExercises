6870582528f26f68b05332836c21e263bde251ac
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-16 10:55:29 -0400

    Create Main.java

---
+++ part10Divisible/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(-17);
+        numbers.add(-5);
+        numbers.add(7);
+
+        ArrayList<Integer> divisible = divisible(numbers);
+
+        divisible.stream()
+                .forEach(num -> System.out.println(num));
+
+    }
+
+
+    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
+        ArrayList<Integer> getDivisible = numbers.stream()
+                .filter(i -> i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return getDivisible;
+    }
+
+}
