652b85ee805d77d1a0a48cec650c98e1bea25a56
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-24 14:41:47 -0400

    Create Main.java

---
+++ part11firstPackages/Main.java
@@ -0,0 +1,11 @@
+import mooc.logic.ApplicationLogic;
+import mooc.ui.UserInterface;
+import mooc.ui.TextInterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new TextInterface();
+        new ApplicationLogic(ui).execute(3);
+    }
+}
