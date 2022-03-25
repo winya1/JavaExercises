4b75d936d0a2d870bf71c2674e30ae3f512ec0aa
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-25 14:55:06 -0400

    Add files via upload

---
+++ part11flightControl/flightControl/domain/Place.java
@@ -0,0 +1,16 @@
+package flightControl.domain;
+
+
+public class Place {
+
+    private String ID;
+
+    public Place(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID;
+    }
+}
