9f0fb23765782186d028a1b6f7a84bc971aa503d
winya1 <50223740+winya1@users.noreply.github.com>
2022-03-30 18:15:24 -0400

    Create Main.java

---
+++ part11sensorsAndTemperature/Main.java
@@ -0,0 +1,24 @@
+import application.AverageSensor;
+import application.Sensor;
+import application.TemperatureSensor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sensor kumpula = new TemperatureSensor();
+        Sensor kaisaniemi = new TemperatureSensor();
+        Sensor helsinkiVantaaAirport = new TemperatureSensor();
+
+        AverageSensor helsinkiRegion = new AverageSensor();
+        helsinkiRegion.addSensor(kumpula);
+        helsinkiRegion.addSensor(kaisaniemi);
+        helsinkiRegion.addSensor(helsinkiVantaaAirport);
+
+        helsinkiRegion.setOn();
+        System.out.println("temperature in Helsinki region " + helsinkiRegion.read() + " degrees Celsius");
+        System.out.println("temperature in Helsinki region " + helsinkiRegion.read() + " degrees Celsius");
+        System.out.println("temperature in Helsinki region " + helsinkiRegion.read() + " degrees Celsius");
+
+        System.out.println("readings: " + helsinkiRegion.readings());
+    }
+}
